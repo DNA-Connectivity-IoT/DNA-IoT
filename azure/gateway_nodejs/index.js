8aa875fddd79af32d86ae66f8619b90160987386
Joni Luomala <joni.luomala@gmail.com>
2019-01-18 10:22:36 +0200

    ruuvitag reader and Jasper integration

--- azure/gateway_nodejs/index.js
+++ azure/gateway_nodejs/index.js
@@ -4,8 +4,10 @@ var Client = require('azure-iot-device').Client;
 var Protocol = require('azure-iot-device-mqtt').Mqtt;
 var Message = require('azure-iot-device').Message;
 var _ = require('lodash');
+var ruuvi = require('./ruuvi.js')
 
-var connectionString = '<IoT hub connection string>';
+//var connectionString = '<IoT hub connection string>';
+var connectionString = 'HostName=DNAControlCenterHub.azure-devices.net;DeviceId=Simulator;SharedAccessKey=dIjQhx/ArSHi79inDfZ24iS3iQbemAI486OP3LzTSRY='
 
 var client = Client.fromConnectionString(connectionString, Protocol);
 
@@ -15,6 +17,7 @@ client.open(function (err) {
     }
     else {
         console.log('client opened');
+
         // Create device Twin
         client.getTwin(function (err, twin) {
             if (err) {
@@ -23,50 +26,49 @@ client.open(function (err) {
             else {
                 console.log('twin created');
                 var iccid = "";
-                twin.on('properties.desired', function (delta) {
+                twin.on('properties.desired', function(delta) {
                     console.log('new desired properties received:');
                     console.log(JSON.stringify(delta.ICCID));
                 });
 
-                twin.on('properties.desired', function (delta) {
+                twin.on('properties.desired', function(delta){
                     var patch = {
-                        ICCID: delta.ICCID,
-                        DataUsage: delta.DataUsage
+                        
+                        DataUsage: delta.dataUsage
                     }
-                    twin.properties.reported.update(patch, function (err) {
-                        if (err) console.log(err)
+                    twin.properties.reported.update(patch, function(err) {
+                        if(err) console.log(err)
                         else console.log('twin state reported')
-                    })
+                    })                    
                 });
                 client.on('message', function (msg) {
-                    console.log('Id: ' + msg.messageId + ' Body: ' + msg.data);
+                    console.log('Id: ' + msg.messageId + ' Body: ' + msg.data); 
                     client.complete(msg, printResultFor('completed'));
-                });
-
-                // Create a message and send it to the IoT Hub every two seconds
-                // Implement sensor reading code here
-                var sendInterval = setInterval(function () {
+                  });
+              
+                  // Create a message and send it to the IoT Hub every two seconds
+                  var sendInterval = setInterval(function () {
                     console.log(twin.properties)
-                    var iccid = twin.properties.reported.ICCID
-                    var dataUsage = twin.properties.reported.DataUsage
-                    var windSpeed = 10 + (Math.random() * 4); // range: [10, 14]
-                    var temperature = 20 + (Math.random() * 10); // range: [20, 30]
-                    var humidity = 60 + (Math.random() * 20); // range: [60, 80]
-                    var data = { deviceId: 'myFirstDevice', windSpeed: windSpeed, temperature: temperature, humidity: humidity, iccid: iccid, dataUsage: dataUsage };
-                    var message = new Message(JSON.stringify(data));
+
+                    var iccid = ''
+                    var ruuvidata = ruuvi.getdata();
+                    ruuvidata['dataUsage'] = twin.properties.reported.DataUsage
+                    ruuvidata['time'] =  Math.floor(Date.now() / 1000)
+                    ruuvidata['deviceId'] = 'Simulator'
+                    var message = new Message(JSON.stringify(ruuvidata));
                     console.log('Sending message: ' + message.getData());
                     client.sendEvent(message, printResultFor('send'));
-                }, 2000);
-
-                client.on('error', function (err) {
+                  }, 2000);
+              
+                  client.on('error', function (err) {
                     console.error(err.message);
-                });
-
-                client.on('disconnect', function () {
+                  });
+              
+                  client.on('disconnect', function () {
                     clearInterval(sendInterval);
                     client.removeAllListeners();
                     client.open(connectCallback);
-                });
+                  });
 
 
             }
@@ -78,7 +80,7 @@ client.open(function (err) {
 
 function printResultFor(op) {
     return function printResult(err, res) {
-        if (err) console.log(op + ' error: ' + err.toString());
-        if (res) console.log(op + ' status: ' + res.constructor.name);
+      if (err) console.log(op + ' error: ' + err.toString());
+      if (res) console.log(op + ' status: ' + res.constructor.name);
     };
-}
+  }
--- azure/gateway_nodejs/ruuvi.js
+++ azure/gateway_nodejs/ruuvi.js
@@ -2,14 +2,14 @@ var ruuvi = require('node-ruuvitag')
 
 var data = {};
 
-var ruuvit = ['Ulkoruuvi', 'Alapohja1', 'Alapohja2']
-var idt = ['e1c1a62d1d3c', 'e35f5b5fbc45', 'fc1bd191c0bc']
+var ruuvit = ['ruuvi1', 'ruuvi2', 'ruuvi3']
+var idt = ['<ruuvi1_tag.id>', '<ruuvi2_tag.id>', '<ruuvi3_tag.id>']
 var data = {}
 ruuvi.on('found', tag => {
     tag.on('updated', data1 => {
         var i = idt.indexOf(tag.id)
-        if(i !== -1) data[ruuvit[i]] = data1
-        else data[tag.id] =data1
+        if (i !== -1) data[ruuvit[i]] = data1
+        else data[tag.id] = data1
     });
 })
 
